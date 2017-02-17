de66c24c37dd0d5bd4452f539aede3aa19369b90
Razvan Fodor <razvan.fodor@gmail.com>
2017-02-17 08:54:41 +0100

    new script to start projectD in linked docker containers

---
+++ startDockerPd.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+mvn clean install
+docker build -t raz/projectd .
+
+#create projectD network
+docker network remove pd_network
+docker network create pd_network
+
+#start mongodb
+docker stop mongo
+docker rm mongo
+docker run -d --name=mongo --network=pd_network mongo
+
+#start projectD server container
+docker stop projectd
+docker rm projectd
+docker run -d --name=projectd --network=pd_network -p 8080:8080  raz/projectd
